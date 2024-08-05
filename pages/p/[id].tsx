eeeb9395a03f8fa159760bec6ec345360cb1ce2b
111161226 <fanrin9@gmail.com>
2024-08-06 00:53:16 +0900

    routing for /api/auth/publish

--- pages/p/[id].tsx
+++ pages/p/[id].tsx
@@ -1,9 +1,11 @@
 import prisma from '../../lib/prisma';
 import React from "react"
 import { GetServerSideProps } from "next"
+import Router from 'next/router';
 import ReactMarkdown from "react-markdown"
 import Layout from "../../components/Layout"
 import { PostProps } from "../../components/Post"
+import { useSession } from 'next-auth/react';
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   const post = await prisma.post.findUnique({
@@ -12,7 +14,7 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
     },
     include: {
       author: {
-        select: { name: true },
+        select: { name: true, email: true },
       },
     },
   });
@@ -21,7 +23,20 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   };
 }
 
+async function publishPost(id: string): Promise<void> {
+  await fetch(`/api/publish/${id}`, {
+    method: 'PUT',
+  });
+  await Router.push('/');
+}
+
 const Post: React.FC<PostProps> = (props) => {
+  const { data: session, status } = useSession();
+  if (status === 'loading') {
+    return <div>Authenticating ...</div>;
+  }
+  const userHasValidSession = Boolean(session);
+  const postBelongsToUser = session?.user?.email === props.author?.email;
   let title = props.title
   if (!props.published) {
     title = `${title} (Draft)`
@@ -33,6 +48,9 @@ const Post: React.FC<PostProps> = (props) => {
         <h2>{title}</h2>
         <p>By {props?.author?.name || "Unknown author"}</p>
         <ReactMarkdown children={props.content} />
+        {!props.published && userHasValidSession && postBelongsToUser && (
+          <button onClick={() => publishPost(props.id)}>Publish</button>
+        )}
       </div>
       <style jsx>{`
         .page {
